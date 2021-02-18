b904c3d7a19e6598e0afc842a5414d85794ccf02
dimple23 <dimplechavda23@gmail.com>
2021-02-18 15:58:27 -0500

    added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
   ### Travel-app
-     This project requires a travel app that, at a minimum, obtains a desired trip location & date from the user, and displays weather and an image of the location using information obtained from external APIs.
+     <p>This project requires a travel app that, at a minimum, obtains a desired trip location & date from the user, and displays weather and an image of the location using information obtained from external APIs.</p>
 
   ## Used APIs
 
--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -4,27 +4,22 @@ export const updateUI = (trip, data) => {
         element.remove();
     }
 
-    // remove banner and display trip card
     document.getElementById('banner').style.display = "none";
     document.getElementById('section2').style.display = "flex";
 
-    // update card fields: title (city and country), img, time to departure
     document.getElementById("dest_city").innerHTML = data.city + ", ";
     document.getElementById("dest_country").innerHTML = trip.trip.country;
     document.getElementById('output_img2').src = trip.trip.imgURL;
     document.getElementById("depart_info").innerHTML = trip.trip.departureInfo;
 
-    // update weather forcast: termprature. For all dates
-    let temp = `Weather forcast ${trip.trip.tempMin} - ${trip.trip.tempMax} °C`
+    let temp = `Weather forecast ${trip.trip.tempMin} - ${trip.trip.tempMax} °C`
     document.getElementById("day_temp").innerHTML = temp;
 
-    // update weather forcast: icon + details. Applies for 16 days only
     if (trip.trip.diffDays >=0 && trip.trip.diffDays < 16){
         // create <div> element
         let div = document.createElement('div');
         div.setAttribute('id', 'forcast_details');
         
-        // create sub elements and fill with info (weather description, icon)
         let p = document.createElement('p');
         p.textContent = trip.trip.weatherDescription;
         p.setAttribute('id', 'day_desc');
